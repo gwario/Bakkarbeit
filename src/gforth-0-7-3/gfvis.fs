aeabdb52ecf00f83d8a8397b12a05701de5f5eb5
Mario Gastegger <mario.gastegger@gmail.com>
2014-12-05 11:48:54 +0100

    fix visualization of datstack and floatstack

--- src/gforth-0-7-3/gfvis.fs
+++ src/gforth-0-7-3/gfvis.fs
@@ -1,6 +1,3 @@
-\ create xterm s" xterm -Sab0 -g 150x15" 2,
-\ create pkillxterm s\" sh -c \"pkill -SIGTERM -f \"xterm -Sab0 -g 150x15\"\"" 2,
-\ xterm 2@ w/o open-pipe throw constant visfile-id
 
 create PWD s" PWD" getenv 2,
 s" pwd" system
@@ -25,9 +22,8 @@ s" pwd" system
 	pad count system
 ;
 
-
 : run-ghostview-working ( -- )
-	s" gv --watch " pad +place
+	s" gv --watch " pad place
 	PWD 2@ pad +place
 	s" /trace.ps" pad +place
 	\ pad count system
@@ -43,41 +39,108 @@ s" pwd" system
 	pad count w/o open-pipe throw drop
 ;
 
-\ what's wrong?
+: split ( str len separator len -- tokens count )
+	
+	here >r 2swap
+	
+	begin
+		2dup 2,             \ save this token ( addr len )
+		2over search        \ find next separator
+	while
+		dup negate  here 2 cells -  +!  \ adjust last token length
+		2over nip /string               \ start next search past separator
+	repeat
+	
+	2drop 2drop
+	r>  here over -   ( tokens length )
+	dup negate allot           \ reclaim dictionary
+	2 cells /                  \ turn byte length into token count
+;
+
 : goto-eof ( fid -- )
 	dup ( fid fid ) file-size ( fid size-l size-h wior ) throw ( fid size-l size-h )
 	rot ( size-l size-h fid ) reposition-file ( wior ) throw
 ;
 
+: goto-bof ( fid -- )
+	0 0 rot ( 0 0 fid ) reposition-file ( wior ) throw
+;
+
+: goto-bol ( c-addr n-read fid -- ) \ position at the beginning of the line, the line must be on the stack
+	
+	rot drop swap ( fid n-read ) over ( fid n-read fid ) file-position throw ( fid n-read n-pos-l n-pos-h ) d>s ( fid n-read n-pos )
+	swap ( fid n-pos n-read ) - 1- ( probably cause of newline ) ( fid n-pol ) s>d rot reposition-file throw
+;
+
+: white?  ( char -- flag )  33 u< ;		\ 2002 Is-White
+
+: trim|  ( addr len -- addr len-i ) 		\ 2002 TRIM
+(
+Trim trailing whitespace from the input string.  If the input is
+all whitespace, the result is the left empty string.
+
+Note:  We prefer the name TRIM| to the Tool Belt's TRIM [the
+same in 2000 and 2002], because a more natural meaning for TRIM
+might be to remove both leading and trailing whitespace.  The
+"|" prefix/suffix for "TRIM" was used by George T. Hawkins in
+his 1987 FStrings Package:
+
+http://www-personal.umich.edu/~williams/archive/forth/strings/fstrings/fstrings.txt
+
+We adopt that practice.  TRIM| is equivalent to the phrase:
+
+  BL SKIP-BACK
+)
+  BEGIN ( len) dup WHILE 1- 2dup chars + c@ white? 0= UNTIL 1+ THEN ;
 
 
-\ create trace file
-s" gfvis.ps" r/o open-file throw constant templatefile-id
-s" trace.ps" r/w create-file throw constant tracefile-id
-\ s" %!PS" tracefile-id write-line throw
+: parse-def ( c-addr u s" ...{\s*<number>\s*}..." -- n <number> )
+	s" {" split drop
+	cell+ cell+ ( s" width} ..." ) \ skip the first token
+	2@ s" }" split drop
+	2@ bl skip trim| s>number? 2drop \ skip blanks and trim the first token - the actual number
+;
 
+
+: n>str ( n c-addr -- )
+	>r dup >r
+	( n |r: c-addr n )
+	abs s>d <# #s r> sign #>
+ 	r@ char+ swap dup >r cmove r> r> c!
+;
+
+\ buffer used to convert numbers to strings
+create num$ 64 chars allot
+
+\ line buffer
 9999 constant max-line
 create line-buffer max-line 2 + allot
 
-create boundingbox-def s" %%BoundingBox:" 2,
+create boundingbox-def s" %%BoundingBox: " 2,
 create cellwidth-def s" /cellwidth " 2,
 create wordcellwidth-def s" /wordcellwidth " 2,
 
 variable cellwidth
 variable wordcellwidth
-variable boundingboxwidth
+2variable boundingboxstart
+2variable boundingboxend
 
-: parse-boundingboxwidth ( c-addr u -- boundingboxwidth )
-	2drop
-	610 \ TODO parse for real
+
+: parse-boundingbox ( c-addr u -- x0 y0 x1 y1 )
+	boundingbox-def 2@ nip /string ( s" x1 y1 x2 y2" )
+	s"  " split drop dup
+	2@ s>number? 2drop swap cell+ cell+ dup \ 2drop drops f and does d>s
+	2@ s>number? 2drop swap cell+ cell+ dup
+	2@ s>number? 2drop swap cell+ cell+
+	2@ s>number? 2drop
 ;
 : parse-cellwidth ( c-addr u -- cellwidth )
-	2drop
-	70 \ TODO parse for real
+	( s" {width} ..." )
+	parse-def
 ;
 : parse-wordcellwidth ( c-addr u -- wordcellwidth )
-	2drop
-	50 \ TODO parse for real
+	( s" {width} ..." )
+	parse-def
 ;
 
 \ changes the font format using ansi control characters
@@ -102,63 +165,89 @@ variable boundingboxwidth
 ;
 
 : log-parsed-line ( c-addr-line u-line c-addr-comment u-comment -- )
-	color-blue cr type space ." -> " space
-	color-green type
+	color-green cr type space ." -> " space
+	color-blue type
 	font-normal
 ;
 
-\ copies the template's content to the trace file, parsing the cell width of stack and word as well as the initial boundingbox width
-: copy-template ( -- )
+: log-parsed-value ( value c-addr-comment u-comment -- )
+	color-green cr type .
+	font-normal
+;
+
+: copy-template { to-fid from-fid -- }
+
 	begin
-		line-buffer max-line templatefile-id read-line throw
+		line-buffer max-line from-fid read-line throw
 		
 	while ( n-read )
-	
+		
 		dup line-buffer swap boundingbox-def 2@ starts-with? if ( n-read )
 		
-			dup line-buffer swap parse-boundingboxwidth boundingboxwidth !
+			dup line-buffer swap parse-boundingbox boundingboxend 2! boundingboxstart 2!
 			dup line-buffer swap s" BoundingBox definition found!" log-parsed-line
+			color-green cr ." boundingbox: " boundingboxstart 2@ . . boundingboxend 2@ . . font-normal
 			
 		else ( n-read )
-		
-			dup line-buffer swap cellwidth-def 2@ starts-with? if ( n-read )
 			
+			dup line-buffer swap cellwidth-def 2@ starts-with? if ( n-read )
+				
 				dup line-buffer swap parse-cellwidth cellwidth !
 				dup line-buffer swap s" cellwidth definition found!" log-parsed-line
+				cellwidth @ s" cellwidth: " log-parsed-value
 				
 			else ( n-read )
 			
 				dup line-buffer swap wordcellwidth-def 2@ starts-with? if ( n-read )
 				
 					dup line-buffer swap parse-wordcellwidth wordcellwidth !
+					
 					dup line-buffer swap s" wordcellwidth definition found!" log-parsed-line
+					wordcellwidth @ s" wordcellwidth: " log-parsed-value
 					
 				endif ( n-read )
 			endif ( n-read )
 		endif ( n-read )
 		
-		line-buffer swap tracefile-id write-line throw
-	repeat
-	drop
+		line-buffer swap to-fid write-line throw
+		
+	repeat drop
 ;
 
 \ updates the boundingbox size: adds the cellwidth and the wordcellwidth 
-: update-boundingbox ( -- )
-	\ TODO reposition to the beginning
-	begin
-		line-buffer max-line tracefile-id read-line throw
-	while
-		dup line-buffer swap max-line s" %%BoundingBox:" starts-with? if
-			\ modify line-buffer to change width by 
-			line-buffer swap tracefile-id write-line throw
-		endif
-	repeat
-	drop
+: update-boundingbox { trace-fid -- }
 	
-	goto-eof
+	trace-fid goto-bof
+	
+	begin
+		line-buffer max-line trace-fid read-line throw
+	while
+		dup line-buffer swap boundingbox-def 2@ starts-with? if ( n-read )
+		
+			dup line-buffer swap trace-fid goto-bol ( n-read )
+			
+			boundingbox-def 2@ pad place
+			
+			boundingboxstart 2@ swap
+			num$ n>str num$ count pad +place s"  " pad +place num$ n>str num$ count pad +place s"  " pad +place
+			
+			boundingboxend 2@ swap cellwidth @ + wordcellwidth @ + 2dup swap boundingboxend 2!
+			num$ n>str num$ count pad +place s"  " pad +place num$ n>str num$ count pad +place
+			
+			pad count trace-fid write-file throw
+			
+		endif ( n-read ) drop
+	repeat drop
+	
+	trace-fid goto-eof
 ;
 
-copy-template
+\ create trace file
+s" gfvis.ps" r/o open-file throw constant templatefile-id
+s" trace.ps" r/w create-file throw constant tracefile-id
+
+
+tracefile-id templatefile-id copy-template
 templatefile-id close-file throw
 
 \ set the c variable
@@ -172,10 +261,7 @@ run-ghostview-working
 
 
 : gfvis-close ( -- )
-	\ pkillxterm 2@ w/o open-pipe throw drop
-	\ visfile-id close-file
 	kill-ghostview-working
-	\ kill-ghostview
 	tracefile-id close-file
 ;
 
@@ -184,16 +270,6 @@ run-ghostview-working
     #cr emit cr .s
     #cr emit cr f.s
     #cr emit cr order ;
-
-
-: n>str ( n c-addr -- )
-	>r dup >r
-	( n |r: c-addr n )
-	abs s>d <# #s r> sign #>
- 	r@ char+ swap dup >r cmove r> r> c!
-;
-
-create num$ 64 chars allot
 
 : write-word-def ( word -- )
 	s" /wordname (" pad place
@@ -204,7 +280,7 @@ create num$ 64 chars allot
 : write-datastack-def ( -- )
 	s" /datastack [ " tracefile-id write-file throw
 	
-	depth 0 max maxdepth-.s @ min  \ not more than  maxdepth-.s
+	depth 0 max \ maxdepth-.s @ min  \ not more than  maxdepth-.s
 	dup 0 ?do
 		dup i - pick
 		s" (" pad place
@@ -220,7 +296,7 @@ create num$ 64 chars allot
 : write-floatstack-def ( -- )
 	s" /floatstack [ " tracefile-id write-file throw
 
-	fdepth 0 max maxdepth-.s @ min \ not more than maxdepth-.s
+	fdepth 0 max \ maxdepth-.s @ min \ not more than maxdepth-.s TODO set max depth in dependant of font height and document height / 3
 	dup 0 ?DO
 		dup i - 1- floats fp@ + f@
 		s" (" pad place
@@ -243,7 +319,7 @@ create num$ 64 chars allot
 	write-datastack-def
 	write-floatstack-def
 	write-command
-	\ update-boundingbox throw
+	tracefile-id update-boundingbox
 ;
 
 
